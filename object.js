d3e6a054c65d37b44e497152190baddcd3c835ad
anikmondol <anikmondol558363@gmil.com>
2024-03-18 00:44:15 +0600

    introduction to objects properies and values

---
+++ object.js
@@ -0,0 +1,40 @@
+// primitive
+// basic, primary
+
+const age = 21;
+const school = 'rifle square';
+const isPassed = true;
+let isDeveloper;
+// console.log(isDeveloper);
+
+
+
+// const subject = ['bangle', 'engels', 'math', 'physics'];
+
+
+
+// objects
+// non-primitive 
+
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+
+console.log(bottle);
+
+
+
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda ma',
+    examDate: '30 fed',
+    chapters: ['first', 'seconde', 'third'],
+    exam: {
+        name: 'final',
+        marks: 100
+    }
+}
