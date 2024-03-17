41ec3017bd9ba5133134110bc668f661cd358bbd
anikmondol <anikmondol558363@gmil.com>
2024-03-17 21:44:02 +0600

    intorduction to string and string vs array

---
+++ string.js
@@ -0,0 +1,41 @@
+const country = 'Bangladesh';
+const division = "New khal e";
+const district = `B-Baria`;
+
+const thana = new String('Demra');
+
+
+// console.log(typeof country);
+// console.log(typeof division);
+// console.log(typeof district);
+
+// console.log(thana);
+// console.log(typeof thana);
+
+
+
+
+const numbers = [54, 98, 21, 65, 70];
+console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[1]);
+numbers[1] = 11;
+console.log(numbers);
+
+
+const capital = 'Dhaka';
+// console.log(capital);
+// console.log(capital.length);
+// console.log(capital[3]);
+
+
+console.log(capital);
+capital[1] = "d";
+console.log(capital);
+
+
+
+
+
+// string is immutable ----> not changeable 
+// array is mutable ---> you can change the array
